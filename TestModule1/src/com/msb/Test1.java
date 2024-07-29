671f9001f4b1532096f71a475443dff30ce2e23b
ltz <ltianz18@163.com>
2024-07-29 09:43:15 +0800

    创建了Test1

---
+++ TestModule1/src/com/msb/Test1.java
@@ -0,0 +1,13 @@
+package com.msb;
+
+/**
+ * @Auther: Idealart
+ * @Date: 7/29/2024 - 07 - 29 - 9:41 AM
+ * @Description: com.msb
+ * @version:
+ */
+public class Test1 {
+    public static void main(String[] args) {
+
+    }
+}
